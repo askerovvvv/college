cbbab4e63ac1e238bcbc4687fe38dc0c002de20b
Damir <damirplaneta6526@gmail.com>
2023-02-26 13:58:57 +0600

    information added in readme

---
+++ account/send_mail.py
@@ -0,0 +1,14 @@
+from django.core.mail import send_mail
+
+
+def mail_message(code, email,):
+    # if status == 'register':
+    link = f'http://localhost:8000/api/v1/element/activate/{code}'
+
+    send_mail(
+        'From django project',
+        link,
+        'damirplaneta6526@gmail.com',
+        [email]
+    )
+# Register, urls, Registerserializer, models activation_code
