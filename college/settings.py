b415618a55a91a01fe7a8cb4d88d4727c13f0c2d
Damir <damirplaneta6526@gmail.com>
2023-02-26 14:36:31 +0600

    information added in readme

--- college/settings.py
+++ college/settings.py
@@ -137,7 +137,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bekbol.2019@gmail.com'
+EMAIL_HOST_USER = 'damirplaneta6526@gmail.com'
 EMAIL_HOST_PASSWORD = 'sfvwhicxshfbuefx'
 
 REST_FRAMEWORK = {
